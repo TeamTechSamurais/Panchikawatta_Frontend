8521ebff793578583115ac69ad4493b3edd2a0e5
sheharaF <fernandopusmp.21@uom.lk>
2024-03-09 22:24:45 +0530

    Updated profile page

--- panchikawatta/lib/screens/profile_page.dart
+++ panchikawatta/lib/screens/profile_page.dart
@@ -8,7 +8,20 @@ class profile_page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile Page'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_rounded),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'My Profile',
+          style: TextStyle(
+            color: Color(0xFFFF5C01),
+            fontSize: 27,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
       body: const Center(
         child: Text('Profile Page Content'),
