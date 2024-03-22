9af4200022506dd9bc5b69ae9e67d2d2412c46d6
Shashini <shashiniimasha31@gmail.com>
2024-03-22 20:08:03 +0530

    after separating functions

---
+++ panchikawatta/lib/components/input_fields.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class InputFields extends StatefulWidget {
+  final String hintText;
+  final double width1;
+  final Icon? suffixIcon;
+
+  InputFields({
+    required this.hintText,
+    required this.width1,
+    this.suffixIcon,
+  });
+
+  @override
+  _InputFieldsState createState() => _InputFieldsState();
+}
+
+class _InputFieldsState extends State<InputFields>{
+  
+  String _textFieldValue = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * widget.width1,
+      height: 55,
+      child: TextField(
+        onChanged: (value) {
+          setState(() {
+            _textFieldValue = value;
+          });
+        },
+        decoration: InputDecoration(
+          hintText: widget.hintText,
+          hintStyle: const TextStyle(fontSize: 16, color: Color(0xCC000000), fontWeight: FontWeight.normal),
+          filled: true,
+          fillColor: const Color(0xFFFAFAFA),//0x80FF5C01
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide.none,
+          ),
+          suffixIcon: widget.suffixIcon != null ? IconButton(
+            onPressed: () {
+              // Handle icon tap (e.g., open a date picker)
+            },
+            icon: widget.suffixIcon!,
+          ) : null,
+        ),
+      ),
+    );
+  }
+}
