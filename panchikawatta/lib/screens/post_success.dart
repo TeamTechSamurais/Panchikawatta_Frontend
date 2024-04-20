a642e036b851a2ca460beb71934ae1d4f9ea9d6d
sheharaF <fernandopusmp.21@uom.lk>
2024-04-20 16:55:10 +0530

    Profile pages updated

--- panchikawatta/lib/screens/post_success.dart
+++ panchikawatta/lib/screens/post_success.dart
@@ -40,7 +40,7 @@ class PostSuccess extends StatelessWidget {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const profile_page()),
+                        builder: (context) => const ProfilePage()),
                   );
                 },
               ),
--- panchikawatta/lib/screens/profile_page.dart
+++ panchikawatta/lib/screens/profile_page.dart
@@ -1,10 +1,14 @@
+// ignore_for_file: prefer_const_constructors, use_super_parameters
+
 import 'package:flutter/material.dart';
 import 'package:panchikawatta/components/custom_button.dart';
 import 'package:panchikawatta/screens/adPost1.dart';
+import 'package:panchikawatta/screens/ad_details_services.dart';
+import 'package:panchikawatta/screens/ad_details_sparepart.dart';
+import 'package:panchikawatta/screens/buyer_profile.dart';
 
-// ignore: camel_case_types
-class profile_page extends StatelessWidget {
-  const profile_page({super.key});
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,16 +27,120 @@ class profile_page extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: CustomButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => AdPost1(key: UniqueKey())),
-            );
-          },
-          text: 'Post Ad',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            const CircleAvatar(
+              radius: 70,
+              backgroundImage: AssetImage('assets/images/profileImage.png'),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Anne Fernando',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => BuyerProfile()),
+                      );
+                    },
+                    child: const Text('        Buyer',
+                        style:
+                            TextStyle(fontSize: 18, color: Color(0xFF757575))),
+                  ),
+                  Text('|'),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Seller        ',
+                        style:
+                            TextStyle(fontSize: 18, color: Color(0xFFFF5C01))),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: EdgeInsets.all(5),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: CustomButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AdPost1(key: UniqueKey())),
+                    );
+                  },
+                  text: 'Post Ad',
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: const Divider(
+                color: Color(0x80000000),
+                thickness: 1,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: const Text(
+                      'My Ads',
+                      style: TextStyle(
+                        color: Color(0xFF000000),
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ProfilePage()),
+                      );
+                    },
+                    child: const Text('Spareparts',
+                        style:
+                            TextStyle(fontSize: 18, color: Color(0xFFFF5C01))),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AdDetailsServices()),
+                      );
+                    },
+                    child: const Text('Services',
+                        style:
+                            TextStyle(fontSize: 18, color: Color(0xFFFF5C01))),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 1000,
+              child: AdDetailsSpareparts(),
+            ),
+          ],
         ),
       ),
     );
