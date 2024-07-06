4031ea7b432697e8919597c8d192f8eb6c43a7c1
Wijesekara <drwijesekara99@gmail.com>
2024-07-06 13:37:35 +0530

    update

--- panchikawatta/lib/screens/User/filter_sort.dart
+++ panchikawatta/lib/screens/User/filter_sort.dart
@@ -10,6 +10,7 @@ import 'package:panchikawatta/dropdowns/province.dart';
 import 'package:panchikawatta/dropdowns/vehicle_make.dart';
 import 'package:panchikawatta/dropdowns/vehicle_model.dart';
 import 'package:panchikawatta/screens/search_page.dart';
+import 'package:panchikawatta/screens/search_page1.dart';
 import 'package:panchikawatta/services/get_api_services.dart';
 
 class FilterSortScreen extends StatefulWidget {
@@ -55,7 +56,7 @@ class _FilterSortScreenState extends State<FilterSortScreen> {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => SearchPage(
+          builder: (context) => search_page1(
             ads: ads,
           ),
         ),
